ee61a5c17864a870acc32b65722f3dabfc2822a5
n3n0w <nenovkrasi@gmail.com>
2024-11-25 19:21:21 +0200

    Update

--- MovieStore/MovieStore/Validators/AddMovieRequestValidator.cs
+++ MovieStore/MovieStore/Validators/AddMovieRequestValidator.cs
@@ -7,10 +7,20 @@ namespace MovieStore.Validators
 
     public class AddMovieRequestValitaror : AbstractValidator<AddMovieRequest>
     {
-        public AddMovieRequestValitaror() { }
+        public AddMovieRequestValitaror()
         {
-        RuleFor( x => x.Title)
-        .NotEmpty
+            {
+                RuleFor(x => x.Title)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(100)
+                .MinimumLength(2);
+
+                RuleFor(x => x.Year)
+                    .GreaterThan(1900).WithMessage("Year must be greater than 1900")
+                    .LessThan(2100);
+            }
         }
+    }
 }
 
