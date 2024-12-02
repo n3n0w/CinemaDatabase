538f58fc7d2c3b9993e9926aba9e4986c7681857
n3n0w <nenovkrasi@gmail.com>
2024-12-02 18:31:55 +0200

    update

---
+++ MovieStore/MovieStore/MovieStore.BL/Interfaces/MoviesBLService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MovieStore.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.BL.Interfaces
+{
+    public interface IMovieBLService
+    {
+        public IEnumerable<MovieView> GetDetailedMovies();
+        
+    }
+}
---
+++ MovieStore/MovieStore/MovieStore.Models/Responses/GetFullMovieDetailsResponse.cs
@@ -0,0 +1,14 @@
+﻿using MovieStore.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Models.Responses
+{
+    public class GetFullMovieDetailsResponse
+    {
+        IEnumerable<MovieView> Movies { get; set; } = [];
+    }
+}
