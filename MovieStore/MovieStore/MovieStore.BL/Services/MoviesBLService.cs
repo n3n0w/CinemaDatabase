9c8715756eef9e86ef1e0127178d71cacedddfcb
n3n0w <nenovkrasi@gmail.com>
2024-12-02 19:03:40 +0200

    update

--- MovieStore/MovieStore/MovieStore.BL/Services/MoviesBLService.cs
+++ MovieStore/MovieStore/MovieStore.BL/Services/MoviesBLService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieStore.BL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace MovieStore.BL.Services
 {
     internal class MoviesBLService
     {
+        private readonly IMovieService _movieService;
     }
 }
